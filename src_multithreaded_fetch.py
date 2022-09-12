b3b12f907717ce03be30d92b49edccf6be9317a8
Kelly Christensen <kelly.christensen@sciencespo.fr>
2022-09-12 14:33:57 +0200

    fixed output file for URAL result objects to be public or private

--- src_multithreaded_fetch.py
+++ src_multithreaded_fetch.py
@@ -6,7 +6,8 @@ from minet import multithreaded_fetch
 from configure_data import TestData
 
 OUTPUT_UNRESPONSIVE_URLS_PATHFILE = os.path.join("data", "unresponsiveURL.csv")
-OUTPUT_FETCHED_URLS_PATHFILE = os.path.join("data", "private.ural_results.csv")
+PRIVATE_OUTPUT_FETCHED_URLS_PATHFILE = os.path.join("data", "private.ural_results.csv")
+PUBLIC_OUTPUT_FETCHED_URLS_PATHFILE = os.path.join("data", "example.ural_results.csv")
 List_of_Namedtuples = list[namedtuple]
 
 
@@ -29,8 +30,17 @@ def output_unresponsiveURLs(unresponsive_urls):
 
 
 def output_fetched_ural_data(results):
-    with open(OUTPUT_FETCHED_URLS_PATHFILE, "w", encoding="UTF8") as output_file:
+    output_filename = check_access_to_controlled_data()
+    with open(output_filename, "w", encoding="UTF8") as output_file:
         writer = csv.writer(output_file)
         writer.writerow(["item", "domain", "url", "error", "response", "meta"])
         writer.writerows([[result.item, result.domain, result.url, result.error, result.response, result.meta] 
             for result in results])
+
+
+def check_access_to_controlled_data():
+    if TestData.has_private_testdata:
+        filepath = PRIVATE_OUTPUT_FETCHED_URLS_PATHFILE
+    else:
+        filepath = PUBLIC_OUTPUT_FETCHED_URLS_PATHFILE
+    return filepath
