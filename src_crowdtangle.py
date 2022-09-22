fa06de4362463b56bb96945f2422e866108eeb7e
Kelly Christensen <kelly.christensen@sciencespo.fr>
2022-09-22 09:46:56 +0200

    remove older version of crowdtangle_extract_text()

--- src_crowdtangle.py
+++ src_crowdtangle.py
@@ -1,10 +1,6 @@
-import re
-from collections import namedtuple
-
 import minet.facebook as facebook
 import yaml
 from minet.crowdtangle.client import CrowdTangleAPIClient
-from ural.facebook import FACEBOOK_DOMAIN_RE, is_facebook_url
 from yaml.loader import SafeLoader
 
 from CONSTANTS import MINET_CONFG
@@ -31,18 +27,6 @@ def call_client(client, url):
 # --------------------------------------------------------#
 
 def crowdtangle_extract_text(fetch_objects, Output):
-    output = []
-    config = parse_config()
-    client = CrowdTangleAPIClient(config["token"], config["rate_limit"])
-    for obj in fetch_objects:
-        if re.search(FACEBOOK_DOMAIN_RE, obj.domain):
-            text = call_client(client, obj.url)
-            if text:
-                output.append(Output(FetchResult=obj, text=text))
-    return output     
-
-
-def new_crowdtangle_extract_text(fetch_objects, Output):
     config = parse_config()
     client = CrowdTangleAPIClient(config["token"], config["rate_limit"])
     return [
